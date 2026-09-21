be45263b884e291a1c59a08de4fab3f8de4be0f2
Sahan Sampath <slunial2022@gmail.com>
2026-09-21 11:49:39 +0530

    Secure database configuration and clean dependencies

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,21 @@
-const mysql= require("mysql2");
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-    user: "root",
-    password :"#Admin1234#",
-    database: "student_task_management",
-    port: 3306
-
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
     if (err) {
         console.error("Error connecting to MySQL database:", err);
-          return;
-    } 
+        return;
+    }
 
     console.log("MySQL connected successfully");
 });
+
+module.exports = db;
